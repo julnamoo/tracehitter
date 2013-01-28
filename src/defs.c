3da00ed2cb570b54c13e850b63d8246bb72fd518
Julie Kim <kjulee114@gmail.com>
2013-01-28 02:32:44 -0800

    remove comment block in add_trace_node. add some log msgs. convert log level from LOG_ERR to LOG_DEBUG while developing.

--- src/defs.c
+++ src/defs.c
@@ -10,6 +10,19 @@ void print_proc_list() {
   }
 }
 
+void print_trace_tree(trace_node* trace_tree) {
+  if (trace_tree == NULL) {
+    syslog(LOG_DEBUG, "End of trace_tree");
+    return;
+  }
+  trace *t_ptr = trace_tree->trace;
+  syslog(LOG_DEBUG, "print trace_node in recursively.");
+  syslog(LOG_DEBUG, "pid %ld, fd %ld, fname %s, state %d, rval %ld",
+      t_ptr->pid, t_ptr->fd, t_ptr->fname, t_ptr->state, t_ptr->rval);
+  print_trace_tree(trace_tree->rchild);
+  print_trace_tree(trace_tree->lchild);
+}
+
 int exist_proc_node(int pid) {
   proc_node* ptr;
   for (ptr = proc_list; ptr != NULL; ptr = ptr->next_proc_node) {
@@ -54,10 +67,16 @@ proc_node* find_proc_node(int pid) {
 int exist_trace_node(trace_node *s_ptr, int fd) {
   while (s_ptr != NULL) {
     if (s_ptr->fd == fd) {
+      syslog(LOG_DEBUG, "Success to find trace_node pid %ld, fd %d",
+          s_ptr->trace->pid, fd);
       return TRUE;
     } else if (s_ptr->fd < fd) {
+      syslog(LOG_DEBUG, "go to rchild from pid %ld, fd %d",
+          s_ptr->trace->pid, s_ptr->fd);
       s_ptr = s_ptr->rchild;
     } else {
+      syslog(LOG_DEBUG, "go to lchild from pid %ld, fd %d",
+          s_ptr->trace->pid, s_ptr->fd);
       s_ptr = s_ptr->lchild;
     }
   }
@@ -66,26 +85,31 @@ int exist_trace_node(trace_node *s_ptr, int fd) {
 }
 
 int add_trace_node(int pid, trace_node *new_node) {
+  syslog(LOG_DEBUG, "enter add_trace_node");
   int depth = 0;
   if (exist_proc_node(pid) == FALSE) {
-    syslog(LOG_ERR, "There are no process with %d. Returned", pid);
+    syslog(LOG_DEBUG, "There are no process with %d. Returned", pid);
     return -1;
   }
   proc_node* p_ptr = find_proc_node(pid);
   trace_node* ttree_ptr = p_ptr->trace_tree;
   if (ttree_ptr == NULL) {
     ttree_ptr = new_node;
+    syslog(LOG_DEBUG, "Is it really the first trace_node of %d(pid)?", pid);
     ttree_ptr->rchild = NULL;
     ttree_ptr->lchild = NULL;
+    print_trace_tree(ttree_ptr);
+    return ttree_ptr->fd;
   }
   trace_node* tmp_ptr = ttree_ptr;
   if (exist_trace_node(ttree_ptr, new_node->fd) == TRUE) {
-    syslog(LOG_ERR, "There is already trace_node with pid:%d and fd:%d.\
+    syslog(LOG_DEBUG, "There is already trace_node with pid:%d and fd:%d.\
         Please check your trace logs.", pid, new_node->fd);
+    print_trace_tree(ttree_ptr);
     return -1;
   }
   /** find position of new_node **/
-  syslog(LOG_DEBUG, "Start right traversal from fd:%d", tmp_ptr->fd);
+  syslog(LOG_DEBUG, "Start traversal from fd:%d", tmp_ptr->fd);
   while (tmp_ptr != NULL) {
     if (tmp_ptr->fd < new_node->fd) {
       tmp_ptr = tmp_ptr->rchild;
@@ -99,29 +123,6 @@ int add_trace_node(int pid, trace_node *new_node) {
   tmp_ptr->lchild = NULL;
   syslog(LOG_DEBUG, "new node fd:%d is located in %dth level",
       tmp_ptr->fd, depth);
+  print_trace_tree(p_ptr->trace_tree);
   return tmp_ptr->fd;
-  /* while (1) {
-    for (; tmp_ptr != NULL && tmp_ptr->fd < new_node->fd;
-        tmp_ptr = tmp_ptr->rchild) {
-      syslog(LOG_DEBUG, "Current visit node:%d, target:%d",
-          tmp_ptr->fd, new_node->fd);
-      if (tmp_ptr->rchild == NULL) {
-        tmp_ptr->rchild = new_node;
-        syslog(LOG_DEBUG, "pid:%d, fd:%d is inserted as a rchild of fd:%d",
-            pid, new_node->fd, tmp_ptr->fd);
-        return tmp_ptr->rchild->fd;
-      }
-    }
-    syslog(LOG_DEBUG, "Start left traversal from fd:%d", tmp_ptr->fd);
-    for (; tmp_ptr != NULL && tmp_ptr->fd > new_node->fd;
-        tmp_ptr = tmp_ptr->lchild) {
-      syslog(LOG_DEBUG, "Current visit node:%d, target:%d",
-          tmp_ptr->fd, new_node->fd);
-      if (tmp_ptr->lchild == NULL) {
-        syslog(LOG_DEBUG, "pid:%d, fd:%d is inserted as a lchild of fd:%d",
-            pid, new_node->fd, tmp_ptr->fd);
-        return tmp_ptr->lchild->fd;
-      }
-    }
-  }*/
 }
