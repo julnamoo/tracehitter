d741348efcaf6bf259ec7307b21405177e7dc625
Julie <kjulee114@gmail.com>
2013-05-17 17:34:08 +0900

    Add print numbers requiested size of all files

--- src/main.c
+++ src/main.c
@@ -545,13 +545,19 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  syslog(LOG_DEBUG, "Finish parsing the trace....Go to next step");
-  fprintf(stderr, "Finish parsing the trace....Go to next step\n");
+  syslog(LOG_DEBUG, "Finish parsing the trace....");
   char* tmp = (char*) malloc(sizeof(char) *
       strlen(getcwd(NULL, 0) + strlen("/tmp")));
   memcpy(tmp, getcwd(NULL, 0), sizeof(char) * strlen(getcwd(NULL, 0)));
   strcat(tmp, "/tmp\0");
   print_granularity(tmp);
+  fprintf(stdout, "Total requested bytes of all files\n");
+  fprintf(stdout, "%5d\t%10d\n", 1, p_total[0]);
+  fprintf(stdout, "%5d\t%10d\n", CLL_64, p_total[1]);
+  fprintf(stdout, "%5d\t%10d\n", CLL_128, p_total[2]);
+  fprintf(stdout, "%5d\t%10d\n", BLOCK_512, p_total[3]);
+  fprintf(stdout, "%5d\t%10d\n", PAGE_4K, p_total[4]);
+  fprintf(stdout, "%5d\t%10d\n", p_total[5], p_total[5]);
 
   closelog();
   exit(EXIT_SUCCESS);
